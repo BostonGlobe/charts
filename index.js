7740ddeb062874c51dee9506777f0e5302b367b1
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-17 14:17:17 -0400

    Add es6/linting

---
+++ index.js
@@ -0,0 +1,1 @@
+export { default as baseballSpraychart } from './transforms/baseball-spraychart.js'
---
+++ transforms/baseball-spraychart.js
@@ -0,0 +1,11 @@
+const transform = (data) => {
+
+	console.log({
+		...data,
+		i: 1
+	})
+	return 'this is baseball'
+
+}
+
+export default transform
