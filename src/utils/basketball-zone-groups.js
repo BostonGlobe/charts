c04a6c22d37fa2c0f1f2aa7831f00c6470d33b1e
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-21 10:44:18 -0400

    zone helper function

---
+++ src/utils/basketball-zone-groups.js
@@ -0,0 +1,83 @@
+const zoneGroups = [
+	{
+		name: 'all field goals',
+		zones: [
+			'restricted area',
+			'low post',
+			'high post',
+			'midrange (lower right)',
+			'midrange (lower left)',
+			'midrange (upper right)',
+			'midrange (upper left)',
+			'midrange (upper middle)',
+			'three (right corner)',
+			'three (left corner)',
+			'three (right)',
+			'three (middle)',
+			'three (left)',
+			'three (deep)',
+		]
+	},
+	{
+		name: 'all field goals (excluding restricted area)',
+		zones: [
+		'low post',
+		'high post',
+		'midrange (lower right)',
+		'midrange (lower left)',
+		'midrange (upper right)',
+		'midrange (upper left)',
+		'midrange (upper middle)',
+		'three (right corner)',
+		'three (left corner)',
+		'three (right)',
+		'three (middle)',
+		'three (left)',
+		'three (deep)',
+		]
+	},
+	{
+		name: '2pt field goals',
+		zones: [
+		'restricted area',
+		'low post',
+		'high post',
+		'midrange (lower right)',
+		'midrange (lower left)',
+		'midrange (upper right)',
+		'midrange (upper left)',
+		'midrange (upper middle)',
+		]
+	},
+	{
+		name: 'in the paint',
+		zones: [
+		'restricted area',
+		'low post',
+		'high post',
+		]
+	},
+	{
+		name: 'midrange',
+		zones: [
+		'midrange (lower right)',
+		'midrange (lower left)',
+		'midrange (upper right)',
+		'midrange (upper left)',
+		'midrange (upper middle)',
+		]
+	},
+	{
+		name: '3pt field goals',
+		zones: [
+		'three (right corner)',
+		'three (left corner)',
+		'three (right)',
+		'three (middle)',
+		'three (left)',
+		'three (deep)',
+		]
+	},
+]
+
+export { zoneGroups } 
