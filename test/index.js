a4cc5168ee61527794a0d2a67d0d3ff502631ffc
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-23 18:20:24 -0400

    use mocha for tests

--- test/index.js
+++ test/index.js
@@ -1,15 +1,23 @@
-const test = require('tape')
+const expect = require('chai').expect
 const basketballShotchart = require('./../build/index.js').basketballShotchart
 const fs = require('fs')
 
-test('', (assert) => {
+// test('', (assert) => {
 
+
+
+// 	assert.deepEqual(basketballShotchart(input), output)
+
+// 	assert.end()
+
+// })
+
+
+describe('basketball transforms', () => {
 	const input = JSON.parse(fs.readFileSync('./test/fixtures/input/basketball-shotchart.json', 'utf8'))
-
 	const output = JSON.parse(fs.readFileSync('./test/fixtures/output/basketball-shotchart.json', 'utf8'))
 
-	assert.deepEqual(basketballShotchart(input), output)
-
-	assert.end()
-
+	it('should match input', () => {
+		expect(basketballShotchart(input)).to.deep.equal(output)
+	})
 })
