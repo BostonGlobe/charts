097dcbfc76dcfa945da1f7a8707a876a11888ca2
russellgoldenberg <russellgoldenberg@gmail.com>
2016-03-25 13:35:21 -0400

    refactor test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,30 @@
+const fs = require('fs')
 const expect = require('chai').expect
 const basketballShotchart = require('./../build/index.js').basketballShotchart
-const fs = require('fs')
+const base = './test/fixtures/'
 
 describe('basketball transforms', () => {
-	const input = JSON.parse(fs.readFileSync('./test/fixtures/input/basketball-shotchart.json', 'utf8'))
-	const output = JSON.parse(fs.readFileSync('./test/fixtures/output/basketball-shotchart.json', 'utf8'))
+	const transformFile = 'basketball-shotchart-transform'
+	const transformIn = JSON.parse(fs.readFileSync(`${base}/input/${transformFile}.json`, 'utf8'))
+	const transformOut = JSON.parse(fs.readFileSync(`${base}/output/${transformFile}.json`, 'utf8'))
 
-	it('should match input', () => {
-		expect(basketballShotchart.transform(input)).to.deep.equal(output)
+	it('transform should match', () => {
+		expect(basketballShotchart.transform(transformIn)).to.deep.equal(transformOut)
+	})
+
+	const hedFile = 'basketball-shotchart-hed'
+	const hedIn = JSON.parse(fs.readFileSync(`${base}/input/${hedFile}.json`, 'utf8'))
+	const hedOut = 'Paul Millsap 2015-16'
+
+	it('hed should match', () => {
+		expect(basketballShotchart.hed(hedIn.rows)).to.deep.equal(hedOut)
+	})
+
+	const subhedFile = 'basketball-shotchart-subhed'
+	const subhedIn = JSON.parse(fs.readFileSync(`${base}/input/${subhedFile}.json`, 'utf8'))
+	const subhedOut = 'Effectiveness on all shots through Oct. 27'
+
+	it('subhed should match', () => {
+		expect(basketballShotchart.subhed({ rows: subhedIn.rows })).to.deep.equal(subhedOut)
 	})
 })
