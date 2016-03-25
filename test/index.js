708c4624948ece2b1df916228aeac4e588ef7c49
Gabriel Florit <gabrielflorit@gmail.com>
2016-03-25 14:58:23 -0400

    Formatting

--- test/index.js
+++ test/index.js
@@ -3,28 +3,39 @@ const expect = require('chai').expect
 const basketballShotchart = require('./../build/index.js').basketballShotchart
 const base = './test/fixtures/'
 
+const readJSON = (name) =>
+	JSON.parse(fs.readFileSync(name, 'utf8'))
+
 describe('basketball transforms', () => {
-	const transformFile = 'basketball-shotchart-transform'
-	const transformIn = JSON.parse(fs.readFileSync(`${base}/input/${transformFile}.json`, 'utf8'))
-	const transformOut = JSON.parse(fs.readFileSync(`${base}/output/${transformFile}.json`, 'utf8'))
 
 	it('transform should match', () => {
-		expect(basketballShotchart.transform(transformIn)).to.deep.equal(transformOut)
-	})
 
-	const hedFile = 'basketball-shotchart-hed'
-	const hedIn = JSON.parse(fs.readFileSync(`${base}/input/${hedFile}.json`, 'utf8'))
-	const hedOut = 'Paul Millsap 2015-16'
+		const file = 'basketball-shotchart-transform'
+		const input = readJSON(`${base}/input/${file}.json`)
+		const output = readJSON(`${base}/output/${file}.json`)
+
+		expect(basketballShotchart.transform(input))
+			.to.deep.equal(output)
+	})
 
 	it('hed should match', () => {
-		expect(basketballShotchart.hed({ rows: hedIn.rows })).to.deep.equal(hedOut)
-	})
 
-	const subhedFile = 'basketball-shotchart-subhed'
-	const subhedIn = JSON.parse(fs.readFileSync(`${base}/input/${subhedFile}.json`, 'utf8'))
-	const subhedOut = 'Effectiveness on all shots through Oct. 27'
+		const file = 'basketball-shotchart-hed'
+		const input = readJSON(`${base}/input/${file}.json`)
+		const output = 'Paul Millsap 2015-16'
+
+		expect(basketballShotchart.hed({ rows: input.rows }))
+			.to.deep.equal(output)
+	})
 
 	it('subhed should match', () => {
-		expect(basketballShotchart.subhed({ rows: subhedIn.rows })).to.deep.equal(subhedOut)
+
+		const file = 'basketball-shotchart-subhed'
+		const input = readJSON(`${base}/input/${file}.json`)
+		const output = 'Effectiveness on all shots through Oct. 27'
+
+		expect(basketballShotchart.subhed({ rows: input.rows }))
+			.to.deep.equal(output)
 	})
+
 })
