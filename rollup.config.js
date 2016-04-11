30b0202ad79114afe8b053f1f2d66fc0b23c447c
Gabriel Florit <gabrielflorit@gmail.com>
2016-04-11 17:27:15 -0400

    Finish bb spray chart tests

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,15 @@
 import babel from 'rollup-plugin-babel'
 
 export default {
-	plugins: [ babel({
-		exclude: 'node_modules/**'
-	}) ]
+	plugins: [
+		babel({
+			babelrc: false,
+			presets: ["es2015-rollup"],
+			plugins: [
+				"syntax-object-rest-spread",
+				"transform-object-rest-spread",
+			],
+			exclude: 'node_modules/**'
+		})
+	]
 }
