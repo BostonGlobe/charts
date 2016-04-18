67cdb68200a94e1bca84bf903747ffafb23c722d
russellgoldenberg <russellgoldenberg@gmail.com>
2016-04-18 12:10:26 -0400

    add basketball hed/sub

--- src/transforms/basketball-shotchart.js
+++ src/transforms/basketball-shotchart.js
@@ -1,38 +1,55 @@
 import _ from 'lodash'
 
 const getLatestDate = (rows) => {
-	const sorted = rows.sort((a, b) => (+a.gameDate) - (+b.gameDate))
-	const str = sorted[sorted.length - 1].gameDate
-	const year = +str.substring(0, 4)
-	const month = +str.substring(4, 6) - 1
-	const day = +str.substring(6, 8)
+	const sorted = rows
+		.map(r => r)
+		.sort((a, b) => (new Date(a.gameDateTime)) - (new Date(b.gameDateTime)))
+	const latest = sorted.pop().gameDateTime
 
-	const d = new Date(year, month, day)
-	const dateString = d.toDateString()
+	const dateString = new Date(latest).toDateString()
 	const split = dateString.split(' ')
 	const output = `${split[1]}. ${+split[2]}`
 	return output
 }
 
-const hed = ({ rows, filters }) => {
-	if (rows.length) {
+const hasData = (rows, filters) => rows.length && Object.keys(filters).length
+
+const hed = ({ rows = [], filters = {} }) => {
+	if (hasData(rows, filters)) {
 		// get season
 		const seasonStr = rows[0].season.toString()
 
 		// if more than one player, show team name
-		const who = filters.player ? filters.player : filters.team
+		const who = filters.team ? filters.team.value : filters.player.value
 		const when = `${seasonStr.substring(0, 4)}-${seasonStr.substring(4, 6)}`
 
-		return `${who} ${when}`
+		return `${who}, ${when}`
 	}
 
 	return ''
 }
 
-const subhed = ({ rows }) => {
-	if (rows.length) {
+const subhed = ({ rows = [], filters = {} }) => {
+	// const filterValue = {
+	// 	home: v => (v ? 'at home' : 'on the road'),
+	// 	powerPlay: v => (v ? 'on a power play' : 'on even strength'),
+	// 	opponent: v => (`against the ${v}`),
+	// }
+
+	if (hasData(rows, filters)) {
 		const date = getLatestDate(rows)
-		return `Effectiveness on all shots through ${date}`
+
+		// const keys = Object.keys(filters)
+		// const result = keys.reduce((previous, key) => {
+		// 	if (key !== 'player' && key !== 'team') {
+		// 		const val = filters[key].value
+		// 		const subhedVal = filterValue[key](val)
+		// 		return `${previous} ${subhedVal}`
+		// 	}
+		// 	return previous
+		// }, 'Effectiveness on all shots')
+
+		return `Effectives on all shots through ${date}`
 	}
 
 	return ''
---
+++ test/basketball-shotchart.js
@@ -0,0 +1,58 @@
+import { expect } from 'chai'
+import path from 'path'
+
+import { basketballShotchart } from './../src/index.js'
+import { readJSON } from './utils.js'
+
+const base = 'fixtures'
+
+const input = readJSON(
+	path.join(__dirname, base, 'basketball-shotchart-payload.json'))
+
+describe('basketball', () => {
+
+	it('hed should handle no data or filters', () =>
+		expect(basketballShotchart.hed({}))
+			.to.deep.equal(''))
+
+	it('hed should handle no filters', () =>
+		expect(basketballShotchart.hed({ rows: input.rows }))
+			.to.deep.equal(''))
+
+	it('hed should handle team filter', () =>
+		expect(basketballShotchart.hed({
+			rows: input.rows,
+			filters: {
+				team: {
+					key: 'teamNickname',
+					value: 'Hawks',
+				},
+			},
+		})).to.deep.equal('Hawks, 2015-16'))
+
+	it('hed should handle player filter', () =>
+		expect(basketballShotchart.hed({
+			rows: input.rows,
+			filters: {
+				player: {
+					key: 'player',
+					value: 'Paul Millsap',
+				},
+			},
+		})).to.deep.equal('Paul Millsap, 2015-16'))
+
+	it('subhed should handle no data or filters', () =>
+		expect(basketballShotchart.subhed({}))
+			.to.deep.equal(''))
+
+	it('subhed should handle team or player', () =>
+		expect(basketballShotchart.subhed({
+			rows: input.rows,
+			filters: {
+				team: {
+					key: 'teamNickname',
+					value: 'Hawks',
+				},
+			},
+		})).to.deep.equal('Effectives on all shots through Oct. 27'))
+})
