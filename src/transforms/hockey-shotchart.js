7da157b84c7e7700b82c1ee0bf0ebe77f89fcd30
russellgoldenberg <russellgoldenberg@gmail.com>
2016-04-18 10:30:52 -0400

    hockey hed/subhed

--- src/transforms/hockey-shotchart.js
+++ src/transforms/hockey-shotchart.js
@@ -1,50 +1,47 @@
 import _ from 'lodash'
 
 const getLatestDate = (rows) => {
-	const sorted = rows.sort((a, b) => (+a.gameDate) - (+b.gameDate))
-	const str = sorted[sorted.length - 1].gameDate
-	const year = +str.substring(0, 4)
-	const month = +str.substring(4, 6) - 1
-	const day = +str.substring(6, 8)
+	const sorted = rows.sort((a, b) => (new Date(a.gameDateTime)) - (new Date(b.gameDateTime)))
+	const latest = sorted.pop().gameDateTime
 
-	const d = new Date(year, month, day)
-	const dateString = d.toDateString()
+	const dateString = new Date(latest).toDateString()
 	const split = dateString.split(' ')
 	const output = `${split[1]}. ${+split[2]}`
 	return output
 }
 
-const hed = ({ rows, filters }) => {
-	if (rows.length) {
+const hasData = (rows, filters) => rows.length && Object.keys(filters).length
+
+const hed = ({ rows = [], filters = {} }) => {
+	if (hasData(rows, filters)) {
 		// get season
 		const seasonStr = rows[0].season.toString()
 
 		// if more than one player, show team name
-		const who = filters.player ? filters.player : filters.teamNickname
+		const who = filters.team ? filters.team.value : filters.player.value
 		const when = `${seasonStr.substring(0, 4)}-${seasonStr.substring(4, 6)}`
 
-		return `${who} ${when}`
+		return `${who}, ${when}`
 	}
 
 	return ''
 }
 
-const subhed = ({ rows, filters }) => {
+const subhed = ({ rows = [], filters = {} }) => {
 	const filterValue = {
 		home: v => (v ? 'at home' : 'on the road'),
 		powerPlay: v => (v ? 'on a power play' : 'on even strength'),
-		opponentNickname: v => (`against the ${v}`),
+		opponent: v => (`against the ${v}`),
 	}
 
-	if (rows.length) {
+	if (hasData(rows, filters)) {
 		const date = getLatestDate(rows)
 
 		const keys = Object.keys(filters)
-		const result = keys.reduce((previous, current) => {
-			if (current !== 'player' && current !== 'teamNickname') {
-				console.log(current)
-				const val = filters[current]
-				const subhedVal = filterValue[current](val)
+		const result = keys.reduce((previous, key) => {
+			if (key !== 'player' && key !== 'team') {
+				const val = filters[key].value
+				const subhedVal = filterValue[key](val)
 				return `${previous} ${subhedVal}`
 			}
 			return previous
