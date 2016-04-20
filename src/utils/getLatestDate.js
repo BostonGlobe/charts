981652d43a3aaf5c0bca88ce7975dd9cda387f7d
russellgoldenberg <russellgoldenberg@gmail.com>
2016-04-20 14:03:50 -0400

    only look at gamedatetime for latest date

--- src/utils/getLatestDate.js
+++ src/utils/getLatestDate.js
@@ -1,6 +1,7 @@
 export default (rows) => {
 	const sorted = rows
 		.map(r => r)
+		.filter(r => r.gameDateTime)
 		.sort((a, b) => (new Date(b.gameDateTime)) - (new Date(a.gameDateTime)))
 	const latest = sorted[0].gameDateTime
 
